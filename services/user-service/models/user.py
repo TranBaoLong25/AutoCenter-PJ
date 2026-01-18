ba5ae39be8edbec4234e7fc2ea6b1b4b4ef4bcfa
long <longlv5664@ut.edu.vn>
2026-01-18 08:48:02 +0700

    cn khung

---
+++ services/user-service/models/user.py
@@ -0,0 +1,17 @@
+from app import db
+from werkzeug.security import generate_password_hash, check_password_hash
+
+class User(db.Model):
+    user_id = db.Column(db.Integer, primary_key = True, autoincrement=True)
+    username = db.Column(db.String(100), unique=True, nullable = False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password_hash = db.Column(db.String(255), nullable = False)
+    role = db.Column(db.Enum('Member', 'Admin', name = 'user_roles'), default = "Member")
+    status = db.Column(db.Enum('Active', 'Locked', name='user_statuses'), default = "Active")
+    
+    profile = db.relationship("Profile", back_populates="user", uselist=False, cascade="all, delete-orphan")
+    
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
